f8819e4000bf3ca8c66651dd40ff3081a515dd5f
Dachi Natsvlishvili <dachinat@gmail.com>
2018-01-29 03:02:51 +0400

    Adds support for global and local checkboxes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-//require('babel-polyfill');
-
 const insertAtCaret = require("./insert-at-caret");
 
 class Geokeyboard {
     constructor(selectors, params={}, opts={}) {
         this.selectors = [];
+        this.lastFocus = null;
 
         this.params = Object.assign({
-            hotSwitchKey: 96
+            hotSwitchKey: 96,
+            globalHotSwitch: null,
+            globalCheckbox: null,
         }, params);
 
         this.listen(selectors, opts);
@@ -19,16 +20,16 @@ class Geokeyboard {
         selectors = Array.from(document.querySelectorAll(selectors));
 
         selectors.forEach(selector => {
-            selector = (selector.tagName.toLowerCase() === 'iframe') ?
-                (selector.contentWindow || selector.contentDocument).window : selector;
+            selector = this.constructor.getContext(selector);
 
-            if (!selector.opts) {`
+            if (!selector.opts) {
                 selector.opts = {
                     replaceOnType: true,
                     hotSwitch: true,
                     onChange: null,
+                    checkbox: null,
+                    checkFocus: true,
                     listeners: [],
-                    active: true
                 };
             }
             selector.opts = Object.assign(selector.opts, opts);
@@ -42,8 +43,36 @@ class Geokeyboard {
             });
 
             this.toggleListener(selector, 'hotSwitch', 'keypress', e => {
-                this.constructor._hotSwitch.call(this, e)
+                this.constructor._hotSwitch.call(this, e);
             });
+
+            this.toggleListener(selector, 'checkFocus', 'focus', e => {
+               this.constructor._checkFocus.call(this, e);
+            }, true);
+
+            if (this.params.globalCheckbox) {
+                const globalCheckbox = document.querySelector(this.params.globalCheckbox);
+
+                if (!globalCheckbox.opts) {
+                    globalCheckbox.opts = { watchCheckbox: true, listeners: [], };
+                }
+
+                this.toggleListener(globalCheckbox, 'watchCheckbox', 'change', e => {
+                    this.constructor._watchCheckbox.call(this, e);
+                });
+            }
+
+            if (selector.opts.checkbox) {
+                const checkbox = document.querySelector(selector.opts.checkbox);
+
+                if (!checkbox.opts) {
+                    checkbox.opts = { watchCheckbox: true, listeners: [], };
+                }
+
+                this.toggleListener(checkbox, 'watchCheckbox', 'change', e => {
+                    this.constructor._watchCheckbox.call(this, e);
+                });
+            }
         });
 
         this.selectors = Array.from(new Set(this.selectors.concat(selectors)));
@@ -55,36 +84,91 @@ class Geokeyboard {
         return this;
     }
 
-    toggleListener(selector, listener, type, fn) {
+
+    toggleListener(selector, listener, type, fn, useCapture=false) {
         const index = this.hasListener(selector, listener);
+
         if (selector.opts[listener]) {
             if (index === false) {
-                selector.opts.listeners.push({[listener]: fn});
-                selector.addEventListener(type, this.getListener(selector, listener));
+                this.addListener(selector, listener, type, fn, useCapture)
             }
         } else {
             if (index !== false) {
-                selector.removeEventListener(type, this.getListener(selector, listener));
-                selector.opts.listeners.splice(index, 1);
+                this.removeListener(selector, listener, type, useCapture);
             }
         }
     }
 
+    addListener(selector, listener, type, fn) {
+        selector.opts.listeners.push({[listener]: fn});
+        selector.addEventListener(type, this.getListener(selector, listener));
+    }
+
+    removeListener(selector, listener, type) {
+        selector.removeEventListener(type, this.getListener(selector, listener));
+        selector.opts.listeners.splice(this.hasListener(selector, listener), 1);
+    }
+
     hasListener(selector, listener) {
-        const index = selector.opts.listeners.findIndex(f => f[listener]);
+        const index = selector.opts.listeners.findIndex(f => typeof f[listener] === 'function');
         return index === -1 ? false : index;
     }
 
     getListener(selector, listener) {
-        return selector.opts.listeners.find(f => f[listener])[listener];
+        try {
+            return selector.opts.listeners.find(f => f[listener])[listener];
+        } catch (e) {
+            console.warn(`There is no such listener as '${listener}' for '${selector.outerHTML}'...`);
+        }
+    }
+
+    _enable(selector, enableCheckbox=true) {
+        selector = this.constructor.getContext(selector);
+        selector.opts.replaceOnType = true;
+
+        this.addListener(selector, 'replaceOnType', 'keypress', e => {
+            this.constructor._replaceTyped.call(this, e);
+        });
+
+        if (selector.opts['onChange']) {
+            selector.opts['onChange'].call(this, true);
+        }
+
+        if (selector.opts.checkbox && enableCheckbox) {
+            document.querySelector(selector.opts.checkbox).checked = true;
+        }
+
+        if (this.params.globalCheckbox) {
+            document.querySelector(this.params.globalCheckbox).checked = true;
+        }
+    }
+
+    _disable(selector, disableCheckbox=true) {
+        selector = this.constructor.getContext(selector);
+        selector.opts.replaceOnType = false;
+
+        this.removeListener(selector, 'replaceOnType', 'keypress', this.getListener(selector, 'replaceOnType'));
+
+        if (selector.opts['onChange']) {
+            selector.opts['onChange'].call(this, false);
+        }
+
+        if (selector.opts.checkbox && disableCheckbox) {
+            document.querySelector(selector.opts.checkbox).checked = false;
+        }
+
+        if (this.params.globalCheckbox) {//?
+            document.querySelector(this.params.globalCheckbox).checked = false;
+        }
     }
 
     static _replaceTyped(e) {
-        if (!new RegExp(this.constructor.characterSet.join('|')).test(e.key) || e.key.length > 1
+        if (!new RegExp(this.constructor.characterSet.join('|')).test(e.key) || e.key.length > 1) {
             //|| !this.o.active) {
-            || !e.currentTarget.opts.active) {
+            //|| !e.currentTarget.opts.active) {
             return;
         }
+        console.log('return');
         e.preventDefault();
 
         insertAtCaret(e.currentTarget, String.fromCharCode(this.constructor.characterSet.indexOf(e.key) + 4304));
@@ -104,19 +188,61 @@ class Geokeyboard {
         e.preventDefault();
     }
 
+    static _checkFocus(e) {
+        if (e.currentTarget.opts.checkbox) {
+            document.querySelector(e.currentTarget.opts.checkbox).checked = e.currentTarget.opts.replaceOnType;
+        }
+
+        if (this.params.globalCheckbox) {
+            document.querySelector(this.params.globalCheckbox).checked = e.currentTarget.opts.replaceOnType;
+        }
+
+        this.lastFocus = e.currentTarget;
+    }
+
+    static _watchCheckbox(e) {
+        let selectors = document.querySelector(this.params.globalCheckbox) === e.currentTarget ? this.selectors :
+            this.selectors.filter(selector => {
+                selector = this.constructor.getContext(selector);
+                return (document.querySelector(selector.opts.checkbox) === e.currentTarget);
+            });
+
+        selectors.forEach(s => {
+            e.currentTarget.checked ? this._enable(s) : this._disable(s);
+        });
+
+
+        if (this.lastFocus && selectors.includes(this.lastFocus.frameElement || this.lastFocus)) {
+            this.lastFocus.focus();
+        } else {
+           this.constructor.getContext(selectors[0]).focus();
+        }
+    }
+
     static _hotSwitch(e) {
-        if (e.keyCode === this.params.hotSwitchKey) {
+        if (e.keyCode === this.params.hotSwitchKey || e.which === this.params.hotSwitchKey) {
             this.constructor._toggle.call(this, e.currentTarget);
             e.preventDefault();
         }
     }
 
     static _toggle(selector) {
-        //this.params.active = !this.params.active;
-        selector.opts.active = !selector.opts.active;
+        const index = this.hasListener(selector, 'replaceOnType');
 
-        if (selector.opts['onChange']) {
-            selector.opts['onChange'].call(this, selector.opts.active);//this.params.active);
+        if (index !== false) {
+            if (typeof this.params.globalHotSwitch === 'function') {
+               this.selectors.forEach(s => this._disable(s, s === selector));
+               this.params.globalHotSwitch.call(this, false);
+            } else {
+                this._disable(selector);
+            }
+        } else {
+            if (typeof this.params.globalHotSwitch === 'function') {
+               this.selectors.forEach(s => this._enable(s, s === selector));
+                this.params.globalHotSwitch.call(this, true);
+            } else {
+                this._enable(selector);
+            }
         }
     }
 
@@ -132,6 +258,11 @@ class Geokeyboard {
 
     static get characterSet() {
         return 'abgdevzTiklmnopJrstufqRySCcZwWxjh'.split('');
+    }
+
+    static getContext(selector) {
+        return (selector.tagName.toLowerCase() === 'iframe') ?
+            (selector.contentWindow || selector.contentDocument).window : selector;
     }
 
     // Not implemented
